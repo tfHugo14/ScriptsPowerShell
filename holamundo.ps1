459d2eb23a6dc0ccb2931332528841f87612ac15
Hugot <hugotiessanclmente@gmail.com>
2023-10-31 10:51:36 +0100

    Initial commit

---
+++ holamundo.ps1
@@ -0,0 +1,1 @@
+﻿write-host "hola mundo"
---
+++ programa.ps1
@@ -0,0 +1,9 @@
+﻿#comentario mono-linea
+<#
+comentario
+multi-linea
+#>
+Write-Host "segundo programa"
+$numero = 1
+Write-Host $numero.GetType().Name 
+[String]$Nonumero = 5
